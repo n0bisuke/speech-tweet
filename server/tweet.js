09422ace3a01e9953c454f84a198c385c54d69ae
n0bisuke <rsksound@gmail.com>
2016-03-07 19:41:43 +0900

    first

--- server/tweet.js
+++ server/tweet.js
@@ -4,12 +4,7 @@ const twitter = require('twitter');
 const MilkCocoa = require('milkcocoa');
 const milkcocoa = new MilkCocoa('flagie458akn.mlkcca.com');
 const ds = milkcocoa.dataStore('BSHSBE34');
-const tw = new twitter({
-  consumer_key: 'lytdODfWII8UuJJMP8Op8zscz',
-  consumer_secret: 'siUIonbycG0TfbGZjTjPg6HV5wIEFMxuBOkjyAIqdGU29YmDvm',
-  access_token_key: '706747271715532800-Ud4jR897yfAFDzrSWy4AJZoNQ1lTKri',
-  access_token_secret: '41dHJu4nDC23DJoDoOeTpD8Slpu2IBmon8zM2IB8SAdD0'
-});
+const tw = new twitter(require('./twconfig'));
 
 ds.on('send', (sended) => {
   console.log('受信:',sended);
